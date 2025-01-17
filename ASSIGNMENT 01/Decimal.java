56f568d4a2d60fbac7434e723818fd8f180c0458
Alok Kumar <140330466+ALOK-CST@users.noreply.github.com>
2025-01-18 00:01:39 +0530

    Add Decimal class to calculate and display the product of two float numbers

---
+++ ASSIGNMENT 01/Decimal.java
@@ -0,0 +1,8 @@
+public class Decimal {
+    public static void main(String[] args) {
+        float x = 12.34f;
+        float y = 56.78f;
+        float p = x * y;
+        System.out.println("Product of " +x+" and "+y+" is "+ p);
+    }
+}
