56d8989a2ef27e842be5e0fbe79e831218cec252
Alok Kumar <140330466+ALOK-CST@users.noreply.github.com>
2025-01-18 21:43:16 +0530

    display multiplication table for a  number

---
+++ ASSIGNMENT 01/Table.java
@@ -0,0 +1,13 @@
+public class Table
+{
+    public static void main(String args[])
+    {
+        int n= 5,x= 1;
+        System.out.println("Multiplication Table for "+n+" :-");
+        while(x<=10)
+        {
+            System.out.println(n+" * "+x+" = "+n*x);
+            x++;
+        }
+    }    
+}
