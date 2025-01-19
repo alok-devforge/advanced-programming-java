0c882f53ca312522c78384f391b997cd98ac013d
Alok Kumar <140330466+ALOK-CST@users.noreply.github.com>
2025-01-19 20:07:01 +0530

    Refactor spacing in D_Triangle class for consistency

--- ASSIGNMENT 01/D_Triangle.java
+++ ASSIGNMENT 01/D_Triangle.java
@@ -2,16 +2,16 @@ public class D_Triangle
 {
     public static void main(String[] args)
     {
-        int n = 5;
+        int n= 5;
         for(int x= 1;x<=n;x++)
         {
             for(int s= 4-x;s>=0;s--)
             {
                 System.out.print(" ");
             }
-            for(int y = 1; y <= x; y++)
+            for(int y= 1;y<=x;y++)
             {
-                System.out.print(y + " ");
+                System.out.print(y+" ");
             }
             System.out.println();
         }
