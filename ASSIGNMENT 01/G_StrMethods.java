9750ef70c647b82c989a45f93538981b78203e57
Alok Kumar <140330466+ALOK-CST@users.noreply.github.com>
2025-01-20 22:13:29 +0530

    Fix string manipulation outputs in G_StrMethods class for accuracy

--- ASSIGNMENT 01/G_StrMethods.java
+++ ASSIGNMENT 01/G_StrMethods.java
@@ -5,9 +5,9 @@ public class G_StrMethods
         String str= "alok kumar";
         System.out.println("Length of the string: "+str.length());
         System.out.println("Character present at 7th index: "+str.charAt(7));
-        System.out.println("Index at which K is present: "+str.indexOf('K'));
-        System.out.println("Given string in lower case: "+str.toLowerCase());
-        System.out.println("Replace s with s: "+str.replace('a','s'));
+        System.out.println("Index at which k is present: "+str.indexOf('k'));
+        System.out.println("Given string in upper case: "+str.toUpperCase());
+        System.out.println("Replace a with s: "+str.replace('a','s'));
         System.out.println("Check string is empty or not: "+str.isEmpty());
     }    
 }
