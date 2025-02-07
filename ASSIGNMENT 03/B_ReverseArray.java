6903a56ef49efb11261ebb7b1febf3873801f70b
Alok Kumar <140330466+ALOK-CST@users.noreply.github.com>
2025-02-07 21:37:25 +0530

    array reversal logic into a separate method for improved readability

--- ASSIGNMENT 03/B_ReverseArray.java
+++ ASSIGNMENT 03/B_ReverseArray.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 public class B_ReverseArray
 {
+    static void revArray(int arr[])
+    {
+        int n= arr.length;
+        for(int x= 0;x<n/2;x++)
+        {
+            int temp= arr[x];
+            arr[x]= arr[n-x-1];
+            arr[n-x-1]= temp;
+        }
+    }
     public static void main(String args[])
     {
         Scanner ob= new Scanner(System.in);
@@ -11,12 +21,7 @@ public class B_ReverseArray
         for(int x= 0;x<n;x++)
             arr[x]= ob.nextInt();
         System.out.print("Reversed Array: ");
-        for(int x= 0;x<n/2;x++)
-        {
-            int temp= arr[x];
-            arr[x]= arr[n-x-1];
-            arr[n-x-1]= temp;
-        }
+        revArray(arr);
         for(int x= 0;x<n;x++)
             System.out.print(arr[x]+" ");
     }
