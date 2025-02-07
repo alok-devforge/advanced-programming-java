66118d05373a3cfbc6ef73241d67cf4de2b10e8f
Alok Kumar <140330466+ALOK-CST@users.noreply.github.com>
2025-02-07 21:20:05 +0530

    improve array reversal logic and ensure complete output display

--- ASSIGNMENT 03/B_ReverseArray.java
+++ ASSIGNMENT 03/B_ReverseArray.java
@@ -9,13 +9,15 @@ public class B_ReverseArray
         System.out.print("Enter the numbers in the array: ");
         int arr[]= new int[n];
         for(int x= 0;x<n;x++)
-        {
             arr[x]= ob.nextInt();
-        }
         System.out.print("Reversed Array: ");
-        for(int x= n-1;x>=0;x--)
+        for(int x= 0;x<n/2;x++)
         {
-            System.out.print(arr[x]+" ");
+            int temp= arr[x];
+            arr[x]= arr[n-x-1];
+            arr[n-x-1]= temp;
         }
+        for(int x= 0;x<n;x++)
+            System.out.print(arr[x]+" ");
     }
 }
